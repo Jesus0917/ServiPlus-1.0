37bd817b4769fc160c7fa1fee42415186fe09688
Jesus Garcia <ninojavier32@gmail.com>
2023-11-22 09:14:28 -0500

    rsxdg
    Signed-off-by: Jesus Garcia <ninojavier32@gmail.com>

--- lib/features/user_auth/presentation/pages/WorkerProfileScreen.dart
+++ lib/features/user_auth/presentation/pages/WorkerProfileScreen.dart
@@ -1,8 +1,26 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/features/user_auth/presentation/pages/home_page.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  // Configuración para Android
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('assets/logo.png'); // Reemplaza 'app_icon' con el nombre de tu icono de la aplicación
+
+  final InitializationSettings initializationSettings =
+      InitializationSettings(android: initializationSettingsAndroid);
+
+  await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+  );
+
   runApp(const MyApp());
 }
 
@@ -18,15 +36,22 @@ class MyApp extends StatelessWidget {
 }
 
 class WorkerProfileScreen extends StatelessWidget {
-  const WorkerProfileScreen({Key? key, required this.serviceName}) : super(key: key);
+  const WorkerProfileScreen({
+    Key? key,
+    required this.serviceName,
+    this.imagePath = '',
+    this.price = '',
+  }) : super(key: key);
 
   final String serviceName;
+  final String imagePath;
+  final String price;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-         automaticallyImplyLeading: false,
+        automaticallyImplyLeading: false,
         backgroundColor: Colors.transparent,
         elevation: 0,
         title: const Center(
@@ -45,6 +70,13 @@ class WorkerProfileScreen extends StatelessWidget {
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: AssetImage(imagePath.isEmpty
+                    ? 'assets/usuario-de-perfil.png' // Imagen por defecto
+                    : imagePath),
+              ),
+              SizedBox(height: 20),
               Text(
                 'Nombre: $serviceName',
                 style: const TextStyle(
@@ -53,7 +85,47 @@ class WorkerProfileScreen extends StatelessWidget {
                   color: Colors.black,
                 ),
               ),
-              // ... otros widgets en el perfil del trabajador
+              SizedBox(height: 10),
+              Text(
+                'Precio: $price', // Muestra el precio
+                style: const TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton.icon(
+                onPressed: () {
+                  launch("tel://<+573154746748>"); // Reemplaza <Número de teléfono> con el número que deseas llamar
+
+                },
+                icon: Icon(Icons.phone),
+                label: Text('Llamar'),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () async {
+                      await _mostrarDialogAceptar(context);
+                      _enviarNotificacion();
+                    },
+                    icon: Icon(Icons.check),
+                    label: Text('Aceptar Trabajador'),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.pop(context); // Regresar al home
+                    },
+                    icon: Icon(Icons.arrow_back),
+                    label: Text('Regresar'),
+                  ),
+                ],
+              ),
+              // Agrega más información del trabajador según sea necesario
             ],
           ),
         ),
@@ -61,6 +133,64 @@ class WorkerProfileScreen extends StatelessWidget {
     );
   }
 }
+
+// Función para mostrar un diálogo
+  Future<void> _mostrarDialogAceptar(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Aceptar Trabajador'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('¿Estás seguro de que quieres aceptar a este trabajador?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Aceptar'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Cierra el diálogo
+              },
+            ),
+            TextButton(
+              child: Text('Cancelar'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Cierra el diálogo
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+  // Inicializa FlutterLocalNotificationsPlugin
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+      
+  // Función para enviar notificación
+  Future<void> _enviarNotificacion() async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+      'your channel id', // Cambia esto según tus necesidades
+      'your channel name', // Cambia esto según tus necesidades
+      importance: Importance.max,
+      priority: Priority.high,
+      showWhen: false,
+    );
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Trabajador Aceptado',
+      'Has aceptado a un trabajador',
+      platformChannelSpecifics,
+      payload: 'Trabajador aceptado',
+    );
+  }
 
 class ListaTrabajadores extends StatelessWidget {
   final List<Map<String, String>> workers;
